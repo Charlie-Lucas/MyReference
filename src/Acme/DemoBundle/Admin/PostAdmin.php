ee0b979541fc95867ecb438cd468138d38631273
Charlie <charlielucas.cl@gmail.com>
2014-04-22 10:42:17 +0200

    Preparation de la classe admin

---
+++ src/Acme/DemoBundle/Admin/PostAdmin.php
@@ -0,0 +1,39 @@
+<?php
+namespace Acme\DemoBundle\Admin;
+
+use Sonata\AdminBundle\Admin\Admin;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+
+class PostAdmin extends Admin
+{
+    // Fields to be shown on create/edit forms
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        $formMapper
+            ->add('title', 'text', array('label' => 'Post Title'))
+            ->add('author', 'entity', array('class' => 'Acme\DemoBundle\Entity\User'))
+            ->add('body') //if no type is specified, SonataAdminBundle tries to guess it
+        ;
+    }
+
+    // Fields to be shown on filter forms
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+    {
+        $datagridMapper
+            ->add('title')
+            ->add('author')
+        ;
+    }
+
+    // Fields to be shown on lists
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        $listMapper
+            ->addIdentifier('title')
+            ->add('slug')
+            ->add('author')
+        ;
+    }
+}
