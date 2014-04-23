211291f73d9742ea6874cfd6be6fe617fbcf84c7
Charlie <charlielucas.cl@gmail.com>
2014-04-23 10:33:58 +0200

    Creation de la classe Reference admin

---
+++ src/Acme/MyReferenceBundle/Admin/ReferenceAdmin.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Acme\MyReferenceBundle\Admin;
+
+use Sonata\AdminBundle\Admin\Admin;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+
+class ReferenceAdmin extends Admin
+{
+    // Fields to be shown on create/edit forms
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        $formMapper
+            ->add('title', 'string', array('label' => 'Reference Title'))
+            ->add('image', 'entity', array('class' => 'Application\Sonata\MediaBundle\Entity\Media'))
+            ->add('description')
+            ->add('date','date')
+
+        ;
+    }
+
+    // Fields to be shown on filter forms
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+    {
+        $datagridMapper
+            ->add('title')
+            ->add('description')
+            ->add('image', 'entity', array('class' => 'Application\Sonata\MediaBundle\Entity\Media'))
+        ;
+    }
+
+    // Fields to be shown on lists
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        $listMapper
+            ->addIdentifier('title')
+            ->add('slug')
+            ->add('description')
+        ;
+    }
+}
